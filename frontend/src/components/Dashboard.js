c2ce9265fdb948dbffb3f7bc77672d93972014ab
yash <yashar@gmail.com>
2024-07-09 22:21:13 +0530

    netlify.toml

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -37,7 +37,7 @@ const Dashboard = ({ role }) => {
         setEditing(false);
         setShowSuccessMessage(true);
         setTimeout(() => setShowSuccessMessage(false), 3000);
-        fetchData(); // Re-fetch data to ensure frontend reflects backend changes
+        fetchData(); 
       })
       .catch((error) => console.error("Error updating records:", error));
   };
